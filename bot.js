38ffb42cee48a5e522536002f9c5a506fa3468b8
gamingdudester <gamingdudester@gmail.com>
2018-03-21 14:26:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,9 +120,9 @@ if (~nommand.indexOf("foo"))
   {
       if (message.content.contains(','))
       {
-        const localArgs = args.join.split(',');
-        const keyword = args[0];
-        const themessage  = args[1];
+        //const localArgs = args.join.split(',');
+        //const keyword = args[0];
+        //const themessage  = args[1];
         //client.getMessage(process.env.DMid, process.env.databaseid + '').edit(getMessage(process.env.databaseid, client.getMessage(process.env.databaseid).content + localArgs + ',');
       }//fetchUser(process.env.gamingdudester).  client.
   }
