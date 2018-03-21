e304d17dd70fe636f84b0230ed1bac38d0d1e35c
gamingdudester <gamingdudester@gmail.com>
2018-03-21 18:19:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,7 +126,7 @@ if (~nommand.indexOf("foo"))
         //client.getMessage(process.env.DMid, process.env.databaseid + '').edit(getMessage(process.env.DMid, process.env.databaseid.content + localArgs + ','));                  
         //client.getMessage(message.channel, process.env.databaseid + '').edit(getMessage(message.channel, process.env.databaseid.content + localArgs + ','));                  
         //await message.channel.send("hit");
-        message.channel.fetchMessage(process.env.DMid)
+        message.channel.fetchMessage(message.id)
             .then(message => console.log(message.content))
             .catch(console.log('why'));
         //await message.channel.send("hit");
