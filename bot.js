abdc45d1cdc748fe54494a5743f0fcf16cd9df0d
gamingdudester <gamingdudester@gmail.com>
2018-03-22 19:43:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,8 +55,8 @@ client.on("message", async message => {
     client.channels.get(process.env.databaseChannel).fetchMessages({ limit: 2 })
         .then(messages => 
         { 
-          var keywords = messages.first().edit(messages.first().content.split(',&'));
-          var keycomebacks =  messages.last().edit(messages.last().content.split(',&'));
+          var keywords = new Array(messages.first().edit(messages.first().content.split(',&')));
+          var keycomebacks =  new Array(messages.last().edit(messages.last().content.split(',&')));
         })
         .catch(console.error);
   
