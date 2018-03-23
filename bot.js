996d92ca3db952fbdd4e715b8ef7f93ccaa086e8
gamingdudester <gamingdudester@gmail.com>
2018-03-22 20:26:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ if (~nommand.indexOf("foo"))
         const localArgs = args.join().split(',');
      client.channels.get(process.env.databaseChannel).fetchMessages({ limit: 2 })
         .then(messages => {messages.first().edit(keywords.join() + localArgs[0] + ',&');
-                          messages.first().edit(keycomebacks.join() + localArgs[1] + ',&');})
+                          messages.last().edit(keycomebacks.join() + localArgs[1] + ',&');})
         .catch(console.error);
         
   }
