974d1e25967c1ab134f19d08063582c2b8cd9da7
gamingdudester <gamingdudester@gmail.com>
2018-03-22 20:12:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const Discord = require("discord.js");
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
-//
 // Here we load the config.json file that contains our token and our prefix values. 
 const commandPrefix = process.env.prefix;
 
