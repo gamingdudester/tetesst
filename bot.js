6affc5aa8eaa94a7a60fb1bcaa26b1a4fea45cd0
gamingdudester <gamingdudester@gmail.com>
2018-03-25 05:22:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on("ready", () => {
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
- a
+ 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
@@ -117,7 +117,7 @@ if (~nommand.indexOf("foo"))
              spamInterval = setInterval (function () 
              {
               message.channel.send(spamMessage)
-             }, 1300);
+             }, 2000);
      }
    }
   
