bc31836d2f3776307417a22799fc418adff32ea0
gamingdudester <gamingdudester@gmail.com>
2018-04-11 05:56:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -234,19 +234,8 @@ if (~message.content.indexOf("foo"))
   }
   
   if(command === "purge") {
-    // This command removes all messages from all users in the channel, up to 100.
-    
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    message.channel.send(args[0]);
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    let messagecount = parseInt(args[0]);
+  message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
   }
 });
 
