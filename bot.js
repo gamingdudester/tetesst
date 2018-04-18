6e1ba8a6b2419376a640856cfd1ab4be8bd6e263
gamingdudester <gamingdudester@gmail.com>
2018-04-17 23:04:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 const commandPrefix = process.env.prefix;
 
 client.on("ready", () => {
-  bot.user.setGame('Gay');
+  client.user.setGame('Gay');
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
