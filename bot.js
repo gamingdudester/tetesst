b5b1af95be8dc0c4411d21ae1898c8673921abb1
gamingdudester <gamingdudester@gmail.com>
2018-04-28 08:05:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
- 
+ //
 });
 
  
