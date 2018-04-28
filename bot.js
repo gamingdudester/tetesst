87c620339df49e1da2b87a44b6d1d0ff87820ab5
gamingdudester <gamingdudester@gmail.com>
2018-04-28 10:05:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -212,8 +212,6 @@ if (~message.content.indexOf("foo"))
   }
   
   if(command === "ban") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
     if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
     
