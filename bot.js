507f0a5037ac8a5e149de303d2e52fde7c73ccd8
gamingdudester <gamingdudester@gmail.com>
2018-05-08 10:16:07 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -239,6 +239,7 @@ if (~message.content.indexOf("foo"))
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   if(command === "id") 
+  {
     // And we get the bot to say the thing: 
     message.channel.send(message.channel.id);
   }
