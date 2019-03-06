1a409a17242a615677bde37911a4fdbb79896b18
user_00028 <gamingdudester@gmail.com>
2019-03-05 20:08:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();//
+const client = new Discord.Client();
 const commandPrefix = process.env.prefix;
 
 client.on("ready", () => {
