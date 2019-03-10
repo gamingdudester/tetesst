8ef33b9f7e5b1aa699f0683d6ea9c905c34dae4b
user_00028 <gamingdudester@gmail.com>
2019-03-10 05:25:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,7 @@ client.on("message", async message => {
            //newchannel.position = listofchannelsincurrentguild[i].position;
         }
     }
-     var invite = await newguild.channels[1].createInvite();
+     var invite = await client.guilds.get(newguild.id).channels[1].createInvite();
      await message.channel.send(""+invite.url);
   }
   
