f520a46b37c79e247ec902bd5adbb71cfb3730b7
user_00028 <gamingdudester@gmail.com>
2019-03-10 17:33:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,9 +53,10 @@ client.on("guildMemberAdd", (member) =>{
              client.channels.get(process.env.setcloneserver).fetchMessages({ limit: 1})
              .then(messages => 
              {
-                if(member.guild.id == client.guild.find("id", messages.last().content).id) {   
+                if(member.guild.id.toString() == client.guild.find("id", messages.last().content).id) {   
            var AdminRole = member.guild.createRole({name: "Admin", permissions: ['ADMINISTRATOR'],});
            member.addRole(AdminRole);
+             }
  });
            
 });
