ba0785f6d50a48f95ace7aefad40e303591e0809
user_00028 <gamingdudester@gmail.com>
2019-04-20 13:25:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -192,14 +192,14 @@ var invite = await newguild.channels.find(channel => channel.type == "text").cre
     }
     else{
 
-    var botrole = message.guild.roles.find(r => r.name == client.user.username).position;
+    var botrole = message.guild.roles.find(r => r.name == client.user.username);
 
-    console.log(botrole+"");
+    console.log("bot role position:"+botrole.position + " name:" + botrole.name);
 
     message.channel.guild.createRole({
     name: 'Color:'+message.author.id,
     color: args.join(" ")+"",
-    position: botrole + 1
+    position: botrole.position + 1
 }).then((role)=>{
     message.channel.guild.member(message.author).addRole(role.id);
 }).catch((e)=>{
