00b97696fc954d2b1cac9096b81f6306c1aa0097
user_00028 <gamingdudester@gmail.com>
2019-07-21 12:38:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -419,7 +419,7 @@ if(command == "rps")
   function makeid(length) 
  {
    var result           = '';
-   var characters       = 'GAYgay';
+   var characters       = 'gay';
    var charactersLength = characters.length;
    for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
