f95ecb98e627c2e9fee19a5c07fa3c0fd2f687bb
user_00028 <gamingdudester@gmail.com>
2019-07-20 21:24:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -296,10 +296,15 @@ if(command == "rps")
     //var privateGuild = message.guild.createChannel(userone.name + " and " + usertwo + "'s Game", { 
      //permissionOverwrites: [ {id: message.guild.defaultRole.id,deny: ['VIEW_CHANNEL']},{id: userone.id,allow: ['VIEW_CHANNEL']},{id: usertwo.id,allow: ['VIEW_CHANNEL']}]});
     message.guild.createChannel(message.author.id, 'text', {
-      SEND_MESSAGES: false
-    })
-   .then(updated => console.log(updated.id))
-   .catch(console.error);
+         type: 'text',
+         permissionOverwrites: [{
+           id: message.guild.id,
+           deny: ['MANAGE_MESSAGES'],
+           allow: ['SEND_MESSAGES']
+         }]
+       })
+         .then(console.log)
+         .catch(console.error);
 
      
      
