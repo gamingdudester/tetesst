e246a8901a88b333cf889ba4f13ad9c963830240
user_00028 <gamingdudester@gmail.com>
2020-02-16 00:44:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const commandPrefix = process.env.prefix;
- //
+ 
 var listOfRPSRooms = [];
 var listOfPOs = [];
 var listOfPTs = [];
